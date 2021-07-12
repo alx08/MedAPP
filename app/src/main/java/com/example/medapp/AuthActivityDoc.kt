a5b37b0866a7db5d6386f8c8426564a23cb47711
alx08 <49490819+alx08@users.noreply.github.com>
2021-07-12 15:14:35 -0500

    Cambio menor

--- app/src/main/java/com/example/medapp/AuthActivityDoc.kt
+++ app/src/main/java/com/example/medapp/AuthActivityDoc.kt
@@ -79,7 +79,7 @@ class AuthActivityDoc : AppCompatActivity() {
 
     }
 
-    //Integracion boton google
+    //Integracion boton google - activity
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
     if (requestCode == GOOGLE_SIGN_IN) {
