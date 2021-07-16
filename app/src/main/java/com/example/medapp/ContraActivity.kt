b12a18827016ddc4b4d16f5003dd2f426079519a
alx08 <49490819+alx08@users.noreply.github.com>
2021-07-16 17:52:10 -0500

    Correccion de errores referentes al inicio de sesion

--- app/src/main/java/com/example/medapp/ContraActivity.kt
+++ app/src/main/java/com/example/medapp/ContraActivity.kt
@@ -1,6 +1,8 @@
 package com.example.medapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
@@ -31,6 +33,13 @@ class ContraActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.statusBarColor = ContextCompat.getColor(this, R.color.registrocolor)
         }
+
+        /**Validar no sesiones activas**/
+        val prefs: SharedPreferences.Editor = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.clear()
+        prefs.apply()
+        FirebaseAuth.getInstance().signOut()
+
         supportActionBar!!.hide() ///esconde la barra de accion
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
